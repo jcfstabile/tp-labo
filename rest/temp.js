63195c0874cf6adb256401112a54d490dfc2bc3c
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2019-12-05 15:39:53 -0300

    working devices query in a open session

--- rest/temp.js
+++ rest/temp.js
@@ -2,62 +2,39 @@
 //
 // http://tplabo.duckdns.org/api/session?token=s2jYO74NSYxdxaYerEr8DVTRKxaczeLK
 //
-/**
- * HOW TO Make an HTTP Call - GET
- */
-// options for GET
-//
 const http = require('http');
 
 const data = JSON.stringify({});
-
-
-const credentials = new Buffer.from('coquitas@mail@here:1234');
-
-const encodedCredentials = credentials.toString('base64');
 
 const token = 'XbLZTuujrWS9RRRmamf9qQFPOk5usV4Z';
 
 const options_session = {
   hostname: 'tplabo.duckdns.org',
   port: 80,
-  path: '/api/session?token=' + token , // <-- ok, token only to create session 
+  path: '/api/session?token=' + token , // <-- ok, token only to create session & get cookie
   method: 'GET',
   headers: {
-    //'Content-type': 'application/json',
     'Content-Length': '22',
     'Connection':'keep-alive',
-    //'Authorization': 'Basic ' + encodedCredentials ,
   },
-  body: {'key1':'value', 'k':'v'},
 };
 
 
 const req = http.request(options_session, (res) => {
-  //console.log(options_session)
-  //console.log(res)
-  //console.log(`*** headers ***`)
-  //console.log(res.headers)
-  //console.log(`set-cookie`)
-  //console.log(res.headers['set-cookie'])
-  //const cookie = JSON.stringify({});
-  const cookie = {};
-  cookie['set-cookie'] = res.headers['set-cookie']
-  console.log('\n##### before devices #####' + cookie['set-cookie'])
-  //console.log(`*** statusCode: ${res.statusCode}`)
 
-  res.on('data', (d) => {
-    process.stdout.write(d)
-  })
+  res.on('data', (d) => { process.stdout.write(d) })
 
-  devices(cookie);
+  const cookie = res.headers['set-cookie'];
+  console.log('\n##### response cookie  #####' + cookie);
 
-  //res.on('set-cookie', (c) => { process.stdout.write(c) } )
+  devices(res.headers['set-cookie']);
 
   console.log(`*** End: ***`)
+
 });
 
 function devices(cookie){
+
   const options_devices = {
     hostname: 'tplabo.duckdns.org',
     port: 80,
@@ -67,14 +44,13 @@ function devices(cookie){
       'Content-type': 'application/json',
       'Content-Length': '22',
       'Connection':'keep-alive',
-      'Authorization': 'Basic ' + encodedCredentials ,
+      'Cookie': cookie,
     },
-    body: {'key1':'value', 'k':'v'},
   };
 
   const dev = http.request(options_devices, function(res){
     res.on('data', function(d){ process.stdout.write(d)})
-    console.log('\n##### devices #####' + cookie['set-cookie'])
+    console.log('\n##### devices #####' + cookie)
   })
   dev.on('error', (error) => { console.error(error) });
   dev.write(data);
