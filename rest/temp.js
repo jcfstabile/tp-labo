2675cda13673fce33bd727c2c8d22d7f10bddf77
Juan Carlos Francisco Stabile <jcfstabile@gmail.com>
2019-12-05 15:15:03 -0300

    wip query devices

--- rest/temp.js
+++ rest/temp.js
@@ -11,43 +11,77 @@ const http = require('http');
 
 const data = JSON.stringify({});
 
+
 const credentials = new Buffer.from('coquitas@mail@here:1234');
 
 const encodedCredentials = credentials.toString('base64');
 
 const token = 'XbLZTuujrWS9RRRmamf9qQFPOk5usV4Z';
 
-const options = {
+const options_session = {
   hostname: 'tplabo.duckdns.org',
   port: 80,
   path: '/api/session?token=' + token , // <-- ok, token only to create session 
   method: 'GET',
   headers: {
-    'Authorization': 'Basic ' + encodedCredentials ,
-  }
+    //'Content-type': 'application/json',
+    'Content-Length': '22',
+    'Connection':'keep-alive',
+    //'Authorization': 'Basic ' + encodedCredentials ,
+  },
+  body: {'key1':'value', 'k':'v'},
 };
 
-const cokkie = null;
 
-const req = http.request(options, (res) => {
-  console.log(options)
-  console.log(res)
-  console.log(`*** headers ***`)
-  console.log(res.headers)
-  console.log(`*** statusCode: ${res.statusCode}`)
+const req = http.request(options_session, (res) => {
+  //console.log(options_session)
+  //console.log(res)
+  //console.log(`*** headers ***`)
+  //console.log(res.headers)
+  //console.log(`set-cookie`)
+  //console.log(res.headers['set-cookie'])
+  //const cookie = JSON.stringify({});
+  const cookie = {};
+  cookie['set-cookie'] = res.headers['set-cookie']
+  console.log('\n##### before devices #####' + cookie['set-cookie'])
+  //console.log(`*** statusCode: ${res.statusCode}`)
 
   res.on('data', (d) => {
     process.stdout.write(d)
   })
 
-  //res.on('set-cokkie', (c) => { process.stdout.write(c) } )
+  devices(cookie);
+
+  //res.on('set-cookie', (c) => { process.stdout.write(c) } )
 
   console.log(`*** End: ***`)
 });
 
-req.on('error', (error) => {
-  console.error(error)
-});
+function devices(cookie){
+  const options_devices = {
+    hostname: 'tplabo.duckdns.org',
+    port: 80,
+    path: '/api/devices',
+    method: 'GET',
+    headers: {
+      'Content-type': 'application/json',
+      'Content-Length': '22',
+      'Connection':'keep-alive',
+      'Authorization': 'Basic ' + encodedCredentials ,
+    },
+    body: {'key1':'value', 'k':'v'},
+  };
+
+  const dev = http.request(options_devices, function(res){
+    res.on('data', function(d){ process.stdout.write(d)})
+    console.log('\n##### devices #####' + cookie['set-cookie'])
+  })
+  dev.on('error', (error) => { console.error(error) });
+  dev.write(data);
+  dev.end();
+}
+
+req.on('error', (error) => { console.error(error) });
 
 req.write(data);
 req.end();
